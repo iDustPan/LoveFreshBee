0f02cb06fd7d0512ccb07dd122c87cd2745e2794
徐攀 <xupandper@gmail.com>
2016-06-18 16:14:15 +0800

    tabBa设置完毕

--- LoveFreshBee/LoveFreshBee/XPMainTabController.swift
+++ LoveFreshBee/LoveFreshBee/XPMainTabController.swift
@@ -13,23 +13,26 @@ class XPMainTabController: UITabBarController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        tabBar.tintColor = UIColor.darkGrayColor()
         
        addChildViewControllers()
         
     }
 
     private func addChildViewControllers() {
-        addChildViewController("Home", title: "首页")
-        addChildViewController("FlashMarket", title: "闪电超市")
-        addChildViewController("ShoppingCard", title: "购物车")
-        addChildViewController("Mine", title: "我的")
+        addChildViewController("Home", title: "首页", image: "v2_home")
+        addChildViewController("FlashMarket", title: "闪电超市", image: "v2_order")
+        addChildViewController("ShoppingCard", title: "购物车", image: "shopCart")
+        addChildViewController("Mine", title: "我的", image: "v2_my")
     }
     
-    private func addChildViewController(storyboardName: String, title: String) {
+    private func addChildViewController(storyboardName: String, title: String, image: String) {
         let storyBoard = UIStoryboard(name: storyboardName, bundle: NSBundle.mainBundle())
         let viewController = storyBoard.instantiateInitialViewController()!
         
         viewController.childViewControllers[0].title = title
+        viewController.tabBarItem.image = UIImage(named: image)
+        viewController.tabBarItem.selectedImage = UIImage(named: image + "_r")?.imageWithRenderingMode(.AlwaysOriginal)
         
         addChildViewController(viewController)
     }
