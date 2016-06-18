729496c2562274bd6aa2bcccac32f0ac84b614d8
徐攀 <xupandper@gmail.com>
2016-06-18 15:28:43 +0800

    新建工程

---
+++ LoveFreshBee/LoveFreshBee/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LoveFreshBee
+//
+//  Created by 徐攀 on 16/6/18.
+//  Copyright © 2016年 cn.xupan.www. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
